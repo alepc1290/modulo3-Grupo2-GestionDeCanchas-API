83e34704f0fcc26b160c793742998fafe4669000
Alejandro Costa <costascarlossmo@gmail.com>
2026-03-30 17:48:01 -0300

    arreglo cors

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ import passport from "./config/passport.js"; // Passport Google OAuth
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "http://localhost:5173" || "https://modulo3-grupo2-gestion-de-canchas-f.vercel.app",
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
 }))
